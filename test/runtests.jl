635753fd5ef48561c6851e2a49e7a58a13b089b5
Kind <kind_ya@sc-030285l.intra.dlr.de>
2025-07-30 10:52:34 +0200

    excluded printing in costs from coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     cost = QCFL(model, ansatz, 1, integrator = MidPoint(1e-2))
     g_init = [1,1,2,2,2,3,3]
     g_opt, c_opt = optimize(cost, g_init, print_result=false, show_trace = false)
-    @test c_opt ≈ 0. atol = 1e-16
+    @test c_opt ≈ 0. atol = eps(Float64)
 end 
 
 @testset "Classical state at Γ=0 for TFIM" begin
